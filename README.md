31c89117f3e3d5d5c62256e25179e42d7b6a8000
ArtemPushPop <72671982+ArtemPushPop@users.noreply.github.com>
2022-06-20 11:07:17 +0300

    Done homework 2. (#2)
    HW2

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# ML_production_IZ1
+# ML_production_IZ2
 
-This repository will contain solutions to tasks of "ML in production" subject 
+## _Building your own image_
+For building oun image of online inference for model one need to clone this branch of repository and build docker image from dockerfile. This can be done with next commands:
+
+```
+cd </path/to/your/directory>
+git clone https://github.com/made-ml-in-prod-2022/ArtemPushPop.git --branch homework_2 --single-branch
+cd ArtemPushPop/online_inference/
+sudo docker build -t <name of your image> .
+```
+
+To run container from recently build image run:
+
+```
+docker run -dp 8000:8000 --rm <name of your image>
+```
+
+## _Loading docker inference image_
+
+This part of homework and this branch of repository is dedicated for development of online inference of model, trained in previous homework. Solution to this task is docker image of uvicorn server, hosted on docker hub. In order to run the server on your local machine, one need to do next steps:
+```
+docker pull gotovtsev/ml_prod_2
+docker run -dp 8000:8000 --rm gotovtsev/ml_prod_2
+```
+
+Parametrizing server inside docker container is done by environment variables, for example, to load model artifact from inside docker image, execute next command:
+
+```
+docker run -dp 8000:8000 --rm --env MODEL_LOCATION=local [--env PATH_TO_SERIALIZATION=your_path] gotovtsev/ml_prod_2
+```
+```PATH_TO_SERIALIZATION``` can be ommited because it has default value.
+
+For loading another artifact of model from google disk, execute next command:
+```
+docker run -dp 8000:8000 --rm --env PATH_TO_SERIALIZATION=<your_path> gotovtsev/ml_prod_2
+```
+,where ```<your_path>``` is sha of shareble link from google disk.
+
+For checking that server is functioning, in repository located simple script named ```test_request.py```, run it after the docker container with server is up. In output there will be clear trace.
---
+++ ml_project/features/processing.py
@@ -0,0 +1,69 @@
+import logging
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from omegaconf import DictConfig
+import pandas as pd
+
+log = logging.getLogger(__name__)
+
+
+def process_numerical_features() -> Pipeline:
+    """Generate transforms for numerical columns"""
+    numeric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", StandardScaler()),
+        ]
+    )
+    return numeric_transformer
+
+
+def process_categorical_features() -> Pipeline:
+    """Generate transforms for categorical columns"""
+    categoric_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="constant", fill_value="M")),
+            ("scaler", OneHotEncoder()),
+        ]
+    )
+    return categoric_transformer
+
+
+def preprocess_pipeline(config: DictConfig, dataset: pd.DataFrame, eval=False) -> tuple:
+    if not eval:
+        log.debug("splitting target and dataset")
+        target = dataset.to_numpy()[:, -1]
+        dataset = dataset.iloc[:, :-1:]
+
+    log.debug("creating preprocessor pipeline")
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "num",
+                process_numerical_features(),
+                dataset.select_dtypes(include=["float64", "int64"]).columns,
+            ),
+            (
+                "cat",
+                process_categorical_features(),
+                dataset.select_dtypes(include=["object"]).columns,
+            ),
+        ]
+    )
+
+    # check if eval, then there is no target in dataset,
+    # and not need to train test_split
+    if not eval:
+        log.info("preprocessing dataset, and splitting it for train")
+        return train_test_split(
+            preprocessor.fit_transform(dataset),
+            target,
+            test_size=config.test_size,
+            random_state=config.random_state,
+        )
+    else:
+        log.info("preprocessing dataset for evaluation")
+        return preprocessor.fit_transform(dataset)
